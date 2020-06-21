dcd0ccc2cec136492e395125578a41a1e6d77831
EC2 Default User <ec2-user@ip-172-31-34-31.us-east-2.compute.internal>
2020-06-21 20:11:35 +0000

    mm

--- app/Http/Controllers/AdController.php
+++ app/Http/Controllers/AdController.php
@@ -12,6 +12,7 @@ class AdController extends Controller
         if ((strpos($user_agent, 'iPhone') !== false)) {
             return redirect('https://preaf.jp/pa.do?s=v04031&o=44510&guid=ON'); //MALINE - preaf    
         }
-        return redirect('https://fam-ad.com/ad/p/r?_site=64801&_article=16216'); //トリトモ - fam
+        // return redirect('https://fam-ad.com/ad/p/r?_site=64801&_article=16216'); //トリトモ - fam
+        return redirect('https://www.gamefeat.net/webapi/v1/reportClick?ad_id=8035&site_id=27669'); //イククル - GameFeat
     }
 }
