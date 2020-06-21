d50792c76c195e807030256b380be9cdb112f986
EC2 Default User <ec2-user@ip-172-31-34-31.us-east-2.compute.internal>
2020-06-21 05:25:40 +0000

    mm

--- app/Http/Controllers/AdController.php
+++ app/Http/Controllers/AdController.php
@@ -8,8 +8,8 @@ class AdController extends Controller
 {
     //
     public function os(Request $request) {
-        $user_agent = $request->userAgent();
-        if ($user_agent == 'iPhone') {
+        $user_agent =  $request->header('User-Agent');
+        if ((strpos($user_agent, 'iPhone') !== false)) {
             return redirect('https://preaf.jp/pa.do?s=v04031&o=44510&guid=ON'); //MALINE - preaf    
         }
         return redirect('https://fam-ad.com/ad/p/r?_site=64801&_article=16216'); //トリトモ - fam
